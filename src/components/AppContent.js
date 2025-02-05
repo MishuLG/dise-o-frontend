fa5162dee904c5665031d1a3d2219d92aa231e74
Lendy <lendyjavier04@hotmail.com>
2025-02-04 20:13:15 -0400

    update 2.8.4 ultimo intento con la autenticacion del profile

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { CContainer, CSpinner } from '@coreui/react';
+import Profile from './profile/profile';
 
 
 import routes from '../routes';
@@ -18,7 +19,7 @@ const AppContent = ({ currentUser }) => {
                   path="/profile"
                   exact={route.exact}
                   name={route.name}
-                  element={React.cloneElement(route.element, { currentUser })}
+                  element={<Profile currentUser={currentUser} />}
                 />
               )
             );
